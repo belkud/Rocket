46d49fa4006cd882dcbb3a4c9dab671dd8b54f9b
belkud <vlad-belkud@yandex.ru>
2025-05-15 12:45:58 +0300

    refuel

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import './style.css'
 
 let rocket = document.getElementById('rocket') as HTMLImageElement
 let box_rocket = document.getElementById('box_rocket') as HTMLDivElement
-let fuel = document.getElementById('fuel') as HTMLDivElement
+let fuel = document.getElementById('fuel') as HTMLDivElement //! полоса с топливом
+let fuelScore = document.getElementById('fuelScore') as HTMLDivElement //! счётчик остатка топлива
+let refuel = document.getElementById('refuel') as HTMLDivElement //! кнопка 'заправить'
 
 
 let acc = 0 //! по горизонтали
@@ -25,24 +27,30 @@ document.addEventListener('keydown',(event)=> {
     accFuel+=1
     box_rocket.style.transform =  `translate(${acc}px,${acc2}px)`
     rocket.style.rotate =  0 + 'deg'
-    fuel.style.width =  `${77 - accFuel}px` //! меняем топливо
-    fuel.innerHTML= accFuel
+
+    fuel.style.width =  `${77 - accFuel}px` //! меняем топливную полосу
+    
+    fuelScore.innerHTML= (100 - accFuel*1.3).toFixed(1) + ' %'
+    if (accFuel>=77) { //! создаём условие, чтобы уровень топлива не был отрицательным
+      fuelScore.innerHTML= 0 + ' %' as any
+      refuel.style.display = 'none'
+    }  
   }
   if (event.key == 'ArrowDown' || event.code == 'KeyS') {
     acc2+=10
     box_rocket.style.transform =  `translate(${acc}px,${acc2}px)`
     rocket.style.rotate =  0 + 'deg'
   }
-  
-  
-  
-  console.log(acc);
-  console.log(acc2);
+  if (event.code == 'KeyC') {
+    console.log('123');
+      accFuel=0
+  }
   
 })
 
-
-// fuel.style.width =  50 + 'px'
- 
-
-
+refuel.addEventListener('click', ()=> {
+  accFuel=0
+  fuel.style.width = 80 + 'px'
+  fuelScore.innerHTML= 100 + ' %'
+  
+})
